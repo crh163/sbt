86e853f6f326f773faaff30136980540c4b96697
chenrenhao <981448691@qq.com>
2020-09-23 22:01:24 +0800

    1

---
+++ spb/src/main/java/com/crh/sb913/Sb913Application.java
@@ -0,0 +1,13 @@
+package com.crh.sb913;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb913Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb913Application.class, args);
+    }
+
+}
---
+++ spb/src/test/java/com/crh/sb913/Sb913ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crh.sb913;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb913ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
